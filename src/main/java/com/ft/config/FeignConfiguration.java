a4db92db3e8c0e307b5e618cf4a28cac5b49b683
Nguyen Dinh Trung <nguyendinhtrung141@gmail.com>
2018-02-25 12:11:14 +0700

    Initial application generated by JHipster-4.14.0

---
+++ src/main/java/com/ft/config/FeignConfiguration.java
@@ -0,0 +1,10 @@
+package com.ft.config;
+
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableFeignClients(basePackages = "com.ft")
+public class FeignConfiguration {
+
+}
